cd9db221320a96a1f465856075b8dd365bb3ac8b
FantasticFiasco <mattias.kindborg@gmail.com>
2017-03-04 14:29:39 +0100

    Update documentation

--- src/Serilog.Sinks.Http/Sinks/Http/Private/HttpJsonFormatter.cs
+++ src/Serilog.Sinks.Http/Sinks/Http/Private/HttpJsonFormatter.cs
@@ -146,7 +146,7 @@ namespace Serilog.Sinks.Http.Private
 		private static void LogNonFormattableEvent(LogEvent logEvent, Exception e)
 		{
 			SelfLog.WriteLine(
-				"Event at {0} with message template {1} could not be formatted into JSON for Seq and will be dropped: {2}",
+				"Event at {0} with message template {1} could not be formatted into JSON and will be dropped: {2}",
 				logEvent.Timestamp.ToString("o"),
 				logEvent.MessageTemplate.Text,
 				e);
