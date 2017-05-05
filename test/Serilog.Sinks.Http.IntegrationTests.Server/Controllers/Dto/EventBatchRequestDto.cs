daba1103ad63a9ed9bc55929fe892449190792bd
FantasticFiasco <mattias.kindborg@gmail.com>
2017-05-05 07:42:14 +0200

    rename project

--- test/Serilog.Sinks.Http.IntegrationTests.Server/Controllers/Dto/EventBatchRequestDto.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace Serilog.Sinks.Http.IntegrationTests.Server.Controllers.Dto
-{
-	public class EventBatchRequestDto
-	{
-		public IEnumerable<EventDto> Events { get; set; }
-	}
-}
