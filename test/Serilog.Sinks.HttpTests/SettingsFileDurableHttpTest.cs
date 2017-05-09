cc1387bb6e42927b5b5524aa5d0e1170ed895dc5
FantasticFiasco <mattias.kindborg@gmail.com>
2017-05-09 16:19:17 +0200

    add durable http tests

---
+++ test/Serilog.Sinks.HttpTests/SettingsFileDurableHttpTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog.LogServer;
+
+namespace Serilog
+{
+    public class SettingsFileDurableHttpTest : SinkFixture
+    {
+        public SettingsFileDurableHttpTest()
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings_durable_http.json")
+                .Build();
+
+            Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            HttpClient = TestServerHttpClient.Instance;
+            HttpClient.Client = Server.CreateClient();
+        }
+    }
+}
