2f872922a6b2cf542e892094c9fdd64c09471ce8
FantasticFiasco <mattias.kindborg@gmail.com>
2017-06-25 23:35:43 +0200

    chore: add build logo

--- Build.ps1
+++ Build.ps1
@@ -1,13 +1,14 @@
-echo "build: Build started"
+$logo = (Invoke-WebRequest "https://raw.githubusercontent.com/FantasticFiasco/logo/master/logo.raw").toString();
+Write-Host "$logo" -ForegroundColor Green
 
 Push-Location $PSScriptRoot
 
 if (Test-Path .\artifacts) {
-    echo "build: Cleaning .\artifacts"
+    Write-Host "build: Cleaning .\artifacts"
     Remove-Item .\artifacts -Force -Recurse
 }
 
-echo "build: Install NuGet packages"
+Write-Host "build: Install NuGet packages"
 & dotnet restore --no-cache
 
 $branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
@@ -15,12 +16,12 @@ if ($branch -like "*/*") { $branch = $branch.Substring($branch.LastIndexOf("/") 
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{ $true = ""; $false = "$branch-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
-echo "build: Version suffix is $suffix"
+Write-Host "build: Version suffix is $suffix"
 
 foreach ($src in ls src/*) {
     Push-Location $src
 
-    echo "build: Packaging project in $src"
+    Write-Host "build: Packaging project in $src"
 
     & dotnet pack -c Release -o ..\..\.\artifacts --version-suffix=$suffix
     if ($LASTEXITCODE -ne 0) { exit 1 }    
@@ -31,7 +32,7 @@ foreach ($src in ls src/*) {
 foreach ($test in ls test/*Tests) {
     Push-Location $test
 
-    echo "build: Testing project in $test"
+    Write-Host "build: Testing project in $test"
 
     & dotnet test -c Release
     if ($LASTEXITCODE -ne 0) { exit 2 }
