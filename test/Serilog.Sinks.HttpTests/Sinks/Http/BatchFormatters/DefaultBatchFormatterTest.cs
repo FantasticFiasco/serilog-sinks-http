456903236cd45de13f1b1457e5ca07bbd709b000
FantasticFiasco <mattias.kindborg@gmail.com>
2017-08-11 17:43:59 +0200

    wip

---
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/BatchFormatters/DefaultBatchFormatterTest.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using Serilog.Events;
+using Serilog.Formatting;
+using Serilog.Sinks.Http.LogServer.Controllers.Dto;
+using Serilog.Sinks.Http.TextFormatters;
+using Serilog.Support;
+using Shouldly;
+using System.IO;
+using System.Linq;
+using Xunit;
+
+namespace Serilog.Sinks.Http.BatchFormatters
+{
+    public class DefaultBatchFormatterTest
+    {
+        private readonly LogEvent[] logEvents;
+        private readonly ITextFormatter textFormatter;
+        private readonly StringWriter output;
+
+        public DefaultBatchFormatterTest()
+        {
+            logEvents = new LogEvent[] { Some.DebugEvent(), Some.DebugEvent() };
+            textFormatter = new NormalTextFormatter();
+            output = new StringWriter();
+        }
+
+        [Fact]
+        public void FormatLogEvents()
+        {
+            // Arrange
+            var batchFormatter = new DefaultBatchFormatter();
+
+            // Act
+            batchFormatter.Format(logEvents, textFormatter, output);
+
+            // Assert
+            var eventBatch = JsonConvert.DeserializeObject<EventBatchRequestDto>(output.ToString());
+            eventBatch.Events.Count().ShouldBe(logEvents.Count());
+        }
+
+        [Fact]
+        public void FormatFormattedLogEvents()
+        {
+            // Arrange
+            var batchFormatter = new DefaultBatchFormatter();
+            var formattedLogEvents = logEvents.Select(logEvent =>
+                {
+                    var formattedLogEvent = new StringWriter();
+                    textFormatter.Format(logEvent, formattedLogEvent);
+                    return formattedLogEvent.ToString();
+                });
+
+            // Act
+            batchFormatter.Format(formattedLogEvents, output);
+
+            // Assert
+            var eventBatch = JsonConvert.DeserializeObject<EventBatchRequestDto>(output.ToString());
+            eventBatch.Events.Count().ShouldBe(logEvents.Count());
+        }
+
+        [Fact]
+        public void DropWhenSizeExceedsMaximum()
+        {
+            // Arrange
+            var batchFormatter = new DefaultBatchFormatter(0);
+
+            // Act
+            batchFormatter.Format(logEvents, textFormatter, output);
+
+            // Assert
+            var eventBatch = JsonConvert.DeserializeObject<EventBatchRequestDto>(output.ToString());
+            eventBatch.Events.Count().ShouldBe(0);
+        }
+    }
+}
