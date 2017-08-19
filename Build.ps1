68d50a552ba4f4b8412b247517a09c87198f89b3
Mattias Kindborg @FantasticFiasco <FantasticFiasco@users.noreply.github.com>
2017-08-19 11:23:10 +0200

    chore: build using dotnet cli 2.0

--- Build.ps1
+++ Build.ps1
@@ -1,6 +1,9 @@
 $logo = (Invoke-WebRequest "https://raw.githubusercontent.com/FantasticFiasco/logo/master/logo.raw").toString();
 Write-Host "$logo" -ForegroundColor Green
 
+Write-Host "build: Build started"
+Write-Host "build: dotnet cli v$(dotnet --version)"
+
 Push-Location $PSScriptRoot
 
 if (Test-Path .\artifacts) {
@@ -16,20 +19,28 @@ if ($branch -like "*/*") { $branch = $branch.Substring($branch.LastIndexOf("/") 
 $revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
 $suffix = @{ $true = ""; $false = "$branch-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
-Write-Host "build: Version suffix is $suffix"
+Write-Host "build: Version suffix is '$suffix'"
 
-foreach ($src in ls src/*) {
+foreach ($src in Get-ChildItem src/*) {
     Push-Location $src
 
     Write-Host "build: Packaging project in $src"
 
-    & dotnet pack -c Release -o ..\..\.\artifacts --version-suffix=$suffix
+    if ($suffix -eq "")
+    {
+        & dotnet build -c Release
+        & dotnet pack -c Release --include-symbols -o ..\..\artifacts --no-build
+    }
+    else {
+        & dotnet build -c Release --version-suffix=$suffix
+        & dotnet pack -c Release --include-symbols -o ..\..\artifacts --version-suffix=$suffix --no-build
+    }
     if ($LASTEXITCODE -ne 0) { exit 1 }    
 
     Pop-Location
 }
 
-foreach ($test in ls test/*Tests) {
+foreach ($test in Get-ChildItem test/*Tests) {
     Push-Location $test
 
     Write-Host "build: Testing project in $test"
