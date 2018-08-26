26710196f9e64bf5f9eb712eb36c07e12a7a4551
FantasticFiasco <mattias.kindborg@gmail.com>
2018-08-26 22:32:09 +0200

    test: refactor queue limit test

--- test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Sinks/HttpSinkTest.cs
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Sinks/HttpSinkTest.cs
@@ -34,7 +34,7 @@ namespace Serilog.Sinks.Http.Private.Sinks
             httpClient.Events.ShouldBeEmpty();
         }
 
-        [Fact(Skip = "Will be refactored in the next commit")]
+        [Fact]
         public async Task RespectQueueLimit()
         {
             // Arrange
@@ -50,54 +50,16 @@ namespace Serilog.Sinks.Http.Private.Sinks
                 httpClient);
 
             // Act
-            httpSink.Emit(Some.LogEvent("Event {number}", 1));
-
-            await Task.Delay(TimeSpan.FromSeconds(0.5));
-
             Enumerable
-                .Range(2, 10)
+                .Range(1, 10)
                 .ToList()
                 .ForEach(number => httpSink.Emit(Some.LogEvent("Event {number}", number)));
-
-            await Task.Delay(TimeSpan.FromSeconds(5));
-
-            // Assert
-            httpClient.Events.Length.ShouldBe(2);
-
-            // The first event will always be sent
-            (await httpClient.Events[0].ReadAsStringAsync()).ShouldBe("{\"events\":[Event 1]}");
-
-            // The second event will also be sent, but the rest will be dropped due to the queue limit
-            (await httpClient.Events[1].ReadAsStringAsync()).ShouldBe("{\"events\":[Event 10]}");
-        }
-
-        [Fact]
-        public async Task RespectQueueLimit()
-        {
-            // Arrange
-            var httpClient = new Mock<IHttpClient>();
-
-            var httpSink = new HttpSink(
-                "api/events",
-                1,
-                1,  // Queue only holds 1 event
-                TimeSpan.FromSeconds(2),
-                new NormalRenderedTextFormatter(),
-                new DefaultBatchFormatter(),
-                httpClient.Object);
-
-            // Act
-            for (int i = 0; i < 10; i++)
-            {
-                httpSink.Emit(Some.InformationEvent());
-            }
 
             await Task.Delay(TimeSpan.FromSeconds(4));
 
             // Assert
-            httpClient.Verify(
-                mock => mock.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>()),
-                Times.Exactly(2)); // Two since the first and the last event will be sent, all other will be dropped from the queue
+            httpClient.Events.Length.ShouldBe(1);
+            (await httpClient.Events[0].ReadAsStringAsync()).ShouldBe("{\"events\":[Event 1]}");
         }
     }
 }
