306ce9fba7fab4b230dcf4073b394253bfd414a0
FantasticFiasco <mattias.kindborg@gmail.com>
2018-08-26 16:50:35 +0200

    test: add queue limited test

--- test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Sinks/HttpSinkTest.cs
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Sinks/HttpSinkTest.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Moq;
 using Serilog.Sinks.Http.BatchFormatters;
 using Serilog.Sinks.Http.TextFormatters;
+using Serilog.Support;
 using Xunit;
 
 namespace Serilog.Sinks.Http.Private.Sinks
@@ -32,6 +33,35 @@ namespace Serilog.Sinks.Http.Private.Sinks
             httpClient.Verify(
                 mock => mock.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>()),
                 Times.Never);
+        }
+
+        [Fact]
+        public async Task RespectQueueLimit()
+        {
+            // Arrange
+            var httpClient = new Mock<IHttpClient>();
+
+            var httpSink = new HttpSink(
+                "api/events",
+                1,
+                1,  // Queue only holds 1 event
+                TimeSpan.FromSeconds(2),
+                new NormalRenderedTextFormatter(),
+                new DefaultBatchFormatter(),
+                httpClient.Object);
+
+            // Act
+            for (int i = 0; i < 10; i++)
+            {
+                httpSink.Emit(Some.InformationEvent());
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(4));
+
+            // Assert
+            httpClient.Verify(
+                mock => mock.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>()),
+                Times.Exactly(2)); // Two since the first and the last event will be sent, all other will be dropped from the queue
         }
     }
 }
