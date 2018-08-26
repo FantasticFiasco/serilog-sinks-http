db82582ecd68387f080dd2f3655853322e2d8368
FantasticFiasco <mattias.kindborg@gmail.com>
2018-08-26 16:50:35 +0200

    test: add queue limited test

--- test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Sinks/HttpSinkTest.cs
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Sinks/HttpSinkTest.cs
@@ -70,5 +70,34 @@ namespace Serilog.Sinks.Http.Private.Sinks
             // The second event will also be sent, but the rest will be dropped due to the queue limit
             (await httpClient.Events[1].ReadAsStringAsync()).ShouldBe("{\"events\":[Event 10]}");
         }
+
+        [Fact]
+        public async Task RespectQueueLimit()
+        {
+            // Arrange
+            var httpClient = new Mock<IHttpClient>();
+
+            var httpSink = new HttpSink(
+                "api/events",
+                1,
+                1,  // Queue only holds 1 event
+                TimeSpan.FromSeconds(2),
+                new NormalRenderedTextFormatter(),
+                new DefaultBatchFormatter(),
+                httpClient.Object);
+
+            // Act
+            for (int i = 0; i < 10; i++)
+            {
+                httpSink.Emit(Some.InformationEvent());
+            }
+
+            await Task.Delay(TimeSpan.FromSeconds(4));
+
+            // Assert
+            httpClient.Verify(
+                mock => mock.PostAsync(It.IsAny<string>(), It.IsAny<HttpContent>()),
+                Times.Exactly(2)); // Two since the first and the last event will be sent, all other will be dropped from the queue
+        }
     }
 }
