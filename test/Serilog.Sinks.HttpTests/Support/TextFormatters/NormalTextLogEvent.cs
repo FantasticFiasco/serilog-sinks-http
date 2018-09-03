c9ef561de9a99fde2e4e197fb913256db8f8228b
FantasticFiasco <mattias.kindborg@gmail.com>
2018-09-03 07:31:34 +0200

    wip

--- test/Serilog.Sinks.HttpTests/Support/TextFormatters/NormalTextLogEvent.cs
+++ test/Serilog.Sinks.HttpTests/Support/TextFormatters/NormalTextLogEvent.cs
@@ -1,18 +1,7 @@
-﻿using System;
-
-namespace Serilog.Support.TextFormatters
+﻿namespace Serilog.Support.TextFormatters
 {
-    public class NormalTextLogEvent : IEquatable<NormalTextLogEvent>
+    public class NormalTextLogEvent
     {
         public string RenderedMessage { get; set; }
-
-        public bool Equals(NormalTextLogEvent other) =>
-            other != null && other.RenderedMessage == RenderedMessage;
-
-        public override bool Equals(object obj) =>
-            Equals(obj as NormalTextLogEvent);
-
-        public override int GetHashCode() =>
-            0;
     }
 }
