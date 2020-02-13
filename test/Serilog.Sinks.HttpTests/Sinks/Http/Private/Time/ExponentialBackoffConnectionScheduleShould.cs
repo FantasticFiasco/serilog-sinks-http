fccf04f4a487d523fcccaf774f41d32839b0ad8b
FantasticFiasco <mattias.kindborg@gmail.com>
2020-02-13 23:32:54 +0100

    wip

---
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Time/ExponentialBackoffConnectionScheduleShould.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace Serilog.Sinks.Http.Private.Time
+{
+    public class ExponentialBackoffConnectionScheduleShould
+    {
+        [Fact]
+        public void Test()
+        {
+            var x = new ExponentialBackoffConnectionSchedule(TimeSpan.FromSeconds(2));
+
+            while (true)
+            {
+                var y = x.NextInterval;
+
+                x.MarkFailure();
+            }
+        }
+    }
+}
