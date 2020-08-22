239cb8c375c8b928316f151f0293d2a904dfc949
FantasticFiasco <mattias.kindborg@gmail.com>
2020-08-22 23:41:38 +0200

    wip

--- src/Serilog.Sinks.Http/Sinks/Http/ByteSize.cs
+++ src/Serilog.Sinks.Http/Sinks/Http/ByteSize.cs
@@ -17,18 +17,36 @@ using System.Text;
 namespace Serilog.Sinks.Http
 {
     /// <summary>
-    /// Class 
+    /// Class defining various multipliers of byte. 
     /// </summary>
     public static class ByteSize
     {
+        /// <summary>
+        /// Value representing one byte (B).
+        /// </summary>
         public const long B = 1;
 
+        /// <summary>
+        /// Value representing 1 kilobyte (KB), or kibibyte (KiB) as the unit sometimes is called.
+        /// </summary>
         public const long KB = 1024 * B;
 
+        /// <summary>
+        /// Value representing 1 megabyte (MB), or mebibyte (MiB) as the unit sometimes is called.
+        /// </summary>
         public const long MB = 1024 * KB;
 
+        /// <summary>
+        /// Value representing 1 gigabyte (GB), or gibibyte (GiB) as the unit sometimes is called.
+        /// </summary>
         public const long GB = 1024 * MB;
 
+        /// <summary>
+        /// Returns the number of bytes produced by UTF8 encoding the characters in the specified
+        /// string.
+        /// </summary>
+        /// <param name="text">The string containing the set of characters to encode.</param>
+        /// <returns>The number of bytes produced by encoding the specified characters.</returns>
         public static long From(string text) => Encoding.UTF8.GetByteCount(text);
     }
 }
