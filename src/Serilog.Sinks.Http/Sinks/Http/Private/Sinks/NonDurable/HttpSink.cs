e0254b24183c854416b9c77b8e663d16e9ca61c5
FantasticFiasco <mattias.kindborg@gmail.com>
2021-03-02 08:21:40 +0100

    wip

--- src/Serilog.Sinks.Http/Sinks/Http/Private/Sinks/NonDurable/HttpSink.cs
+++ src/Serilog.Sinks.Http/Sinks/Http/Private/Sinks/NonDurable/HttpSink.cs
@@ -15,14 +15,12 @@
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 using Serilog.Core;
 using Serilog.Debugging;
 using Serilog.Events;
 using Serilog.Formatting;
-using Serilog.Sinks.PeriodicBatching;
+using Serilog.Sinks.Http.Private.Time;
 
 namespace Serilog.Sinks.Http.Private.Sinks.NonDurable
 {
@@ -30,16 +28,19 @@ namespace Serilog.Sinks.Http.Private.Sinks.NonDurable
     /// A non-durable sink that sends log events using HTTP POST over the network. A non-durable
     /// sink will lose data after a system or process restart.
     /// </summary>
-    /// <seealso cref="PeriodicBatchingSink" />
-    public class HttpSink : ILogEventSink, IBatchedLogEventSink, IDisposable
+    public class HttpSink : ILogEventSink, IDisposable
     {
         private const string ContentType = "application/json";
 
         private readonly string requestUri;
+        private readonly int batchPostingLimit;
+        private readonly long batchSizeLimitBytes;
         private readonly ITextFormatter textFormatter;
         private readonly IBatchFormatter batchFormatter;
         private readonly IHttpClient httpClient;
-        private readonly PeriodicBatchingSink sink;
+        private readonly ExponentialBackoffConnectionSchedule connectionSchedule;
+        private readonly PortableTimer timer;
+        private readonly LogEventQueue<LogEvent> queue;
 
         /// <summary>
         /// Initializes a new instance of the <see cref="HttpSink"/> class.
@@ -55,53 +56,58 @@ namespace Serilog.Sinks.Http.Private.Sinks.NonDurable
             IHttpClient httpClient)
         {
             this.requestUri = requestUri ?? throw new ArgumentNullException(nameof(requestUri));
+            this.batchPostingLimit = batchPostingLimit;
+            this.batchSizeLimitBytes = batchSizeLimitBytes;
             this.textFormatter = textFormatter ?? throw new ArgumentNullException(nameof(textFormatter));
             this.batchFormatter = batchFormatter ?? throw new ArgumentNullException(nameof(batchFormatter));
             this.httpClient = httpClient ?? throw new ArgumentNullException(nameof(httpClient));
 
-            // TODO: Respect batchSizeLimitBytes
-
-            sink = new PeriodicBatchingSink(this, new PeriodicBatchingSinkOptions
-            {
-                BatchSizeLimit = batchPostingLimit,
-                Period = period,
-                QueueLimit = queueLimit
-            });
+            connectionSchedule = new ExponentialBackoffConnectionSchedule(period);
+            timer = new PortableTimer(OnTick);
+            queue = new LogEventQueue<LogEvent>(queueLimit);
         }
 
         /// <inheritdoc />
         public void Emit(LogEvent logEvent)
         {
-            sink.Emit(logEvent);
-        }
+            if (logEvent == null) throw new ArgumentNullException(nameof(logEvent));
 
-        /// <inheritdoc />
-        public async Task EmitBatchAsync(IEnumerable<LogEvent> logEvents)
-        {
-            var payload = FormatPayload(logEvents);
-            var content = new StringContent(payload, Encoding.UTF8, ContentType);
-
-            var result = await httpClient
-                .PostAsync(requestUri, content)
-                .ConfigureAwait(false);
-
-            if (!result.IsSuccessStatusCode)
+            bool success = queue.TryEnqueue(logEvent);
+            if (!success)
             {
-                throw new LoggingFailedException($"Received failed result {result.StatusCode} when posting events to {requestUri}");
+                SelfLog.WriteLine("The queue has reached its limit and log event will be dropped");
             }
-        }
-
-        public Task OnEmptyBatchAsync()
-        {
-            return Task.FromResult<bool>(true);
         }
 
         /// <inheritdoc />
         public void Dispose()
         {
-            sink?.Dispose();
+            timer?.Dispose();
             httpClient?.Dispose();
         }
+
+        // /// <inheritdoc />
+        // public async Task EmitBatchAsync(IEnumerable<LogEvent> logEvents)
+        // {
+        //     var payload = FormatPayload(logEvents);
+        //     var content = new StringContent(payload, Encoding.UTF8, ContentType);
+
+        //     var result = await httpClient
+        //         .PostAsync(requestUri, content)
+        //         .ConfigureAwait(false);
+
+        //     if (!result.IsSuccessStatusCode)
+        //     {
+        //         throw new LoggingFailedException($"Received failed result {result.StatusCode} when posting events to {requestUri}");
+        //     }
+        // }
+
+        // public Task OnEmptyBatchAsync()
+        // {
+        //     return Task.FromResult<bool>(true);
+        // }
+
+        private Task OnTick() => throw new NotImplementedException();
 
         private string FormatPayload(IEnumerable<LogEvent> logEvents)
         {
