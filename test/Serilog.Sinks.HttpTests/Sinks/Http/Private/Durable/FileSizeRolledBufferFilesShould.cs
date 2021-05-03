52da5798ea91fc83f49ef863fd69c959fe96a57b
FantasticFiasco <mattias@kindb.org>
2021-05-03 22:34:30 +0200

    wip

--- test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Durable/FileSizeRolledBufferFilesShould.cs
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Durable/FileSizeRolledBufferFilesShould.cs
@@ -14,7 +14,7 @@ namespace Serilog.Sinks.Http.Private.Durable
         public FileSizeRolledBufferFilesShould()
         {
             directoryService = new Mock<IDirectoryService>();
-            bufferFiles = new FileSizeRolledBufferFiles(directoryService.Object, "Buffer");
+            bufferFiles = new FileSizeRolledBufferFiles(directoryService.Object, "SomeBuffer");
         }
 
         [Fact]
@@ -23,17 +23,17 @@ namespace Serilog.Sinks.Http.Private.Durable
             // Arrange
             var want = new[]
             {
-                "Buffer-20001020.json",
-                "Buffer-20001020_001.json",
-                "Buffer-20001020_002.json",
-                "Buffer-20001020_003.json",
-                "Buffer-20001020_004.json",
-                "Buffer-20001020_005.json",
-                "Buffer-20001020_006.json",
-                "Buffer-20001020_007.json",
-                "Buffer-20001020_008.json",
-                "Buffer-20001020_009.json",
-                "Buffer-20001020_010.json"
+                "SomeBuffer-20001020.json",
+                "SomeBuffer-20001020_001.json",
+                "SomeBuffer-20001020_002.json",
+                "SomeBuffer-20001020_003.json",
+                "SomeBuffer-20001020_004.json",
+                "SomeBuffer-20001020_005.json",
+                "SomeBuffer-20001020_006.json",
+                "SomeBuffer-20001020_007.json",
+                "SomeBuffer-20001020_008.json",
+                "SomeBuffer-20001020_009.json",
+                "SomeBuffer-20001020_010.json"
             };
 
             directoryService
@@ -53,9 +53,9 @@ namespace Serilog.Sinks.Http.Private.Durable
             // Arrange
             var want = new[]
             {
-                "Buffer-20001020_999.json",
-                "Buffer-20001020_1000.json",
-                "Buffer-20001020_1001.json"
+                "SomeBuffer-20001020_999.json",
+                "SomeBuffer-20001020_1000.json",
+                "SomeBuffer-20001020_1001.json"
             };
 
             directoryService
@@ -75,9 +75,9 @@ namespace Serilog.Sinks.Http.Private.Durable
             // Arrange
             var want = new[]
             {
-                "Buffer-20001020_9999.json",
-                "Buffer-20001020_10000.json",
-                "Buffer-20001020_10001.json"
+                "SomeBuffer-20001020_9999.json",
+                "SomeBuffer-20001020_10000.json",
+                "SomeBuffer-20001020_10001.json"
             };
 
             directoryService
