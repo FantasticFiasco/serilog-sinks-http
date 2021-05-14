9cc81c47c9dfda593d90eff64eb550b00c65c73f
FantasticFiasco <mattias@kindb.org>
2021-05-14 17:49:14 +0200

    wip

--- test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Durable/TimeRolledBufferFilesShould.cs
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Durable/TimeRolledBufferFilesShould.cs
@@ -41,6 +41,35 @@ namespace Serilog.Sinks.Http.Private.Durable
         }
 
         [Fact]
+        public void HandleYearsDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "Buffer-2008.json",
+                "Buffer-2009.json",
+                "Buffer-2010.json",
+                "Buffer-2011.json",
+                // "txt" is used from >= v8
+                "Buffer-2008.txt",
+                "Buffer-2009.txt",
+                "Buffer-2010.txt",
+                "Buffer-2011.txt"
+            };
+
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Fact]
         public void HandleMonths()
         {
             // Arrange
@@ -50,6 +79,35 @@ namespace Serilog.Sinks.Http.Private.Durable
                 "Buffer-200112.json",
                 "Buffer-200201.json",
                 "Buffer-200202.json"
+            };
+
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Fact]
+        public void HandleMonthsDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "Buffer-200111.json",
+                "Buffer-200112.json",
+                "Buffer-200201.json",
+                "Buffer-200202.json",
+                // "txt" is used from >= v8
+                "Buffer-200111.txt",
+                "Buffer-200112.txt",
+                "Buffer-200201.txt",
+                "Buffer-200202.txt"
             };
 
             directoryService
@@ -87,6 +145,35 @@ namespace Serilog.Sinks.Http.Private.Durable
         }
 
         [Fact]
+        public void HandleDaysDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "Buffer-20011230.json",
+                "Buffer-20011231.json",
+                "Buffer-20020101.json",
+                "Buffer-20020102.json",
+                // "txt" is used from >= v8
+                "Buffer-20011230.txt",
+                "Buffer-20011231.txt",
+                "Buffer-20020101.txt",
+                "Buffer-20020102.txt"
+            };
+
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Fact]
         public void HandleHours()
         {
             // Arrange
@@ -110,6 +197,35 @@ namespace Serilog.Sinks.Http.Private.Durable
         }
 
         [Fact]
+        public void HandleHoursDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "Buffer-2001123122.json",
+                "Buffer-2001123123.json",
+                "Buffer-2002010100.json",
+                "Buffer-2002010101.json",
+                // "txt" is used from >= v8
+                "Buffer-2001123122.txt",
+                "Buffer-2001123123.txt",
+                "Buffer-2002010100.txt",
+                "Buffer-2002010101.txt"
+            };
+
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Fact]
         public void HandleMinutes()
         {
             // Arrange
@@ -119,6 +235,35 @@ namespace Serilog.Sinks.Http.Private.Durable
                 "Buffer-200112312359.json",
                 "Buffer-200201010000.json",
                 "Buffer-200201010001.json"            };
+
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Fact]
+        public void HandleMinutesDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "Buffer-200112312358.json",
+                "Buffer-200112312359.json",
+                "Buffer-200201010000.json",
+                "Buffer-200201010001.json",
+                // "txt" is used from >= v8
+                "Buffer-200112312358.txt",
+                "Buffer-200112312359.txt",
+                "Buffer-200201010000.txt",
+                "Buffer-200201010001.txt"
+            };
 
             directoryService
                 .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
