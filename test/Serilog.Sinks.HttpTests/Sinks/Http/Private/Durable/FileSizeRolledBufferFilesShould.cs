b6b108755f9fdbe1c7b0a4150f63ac2e8a111254
FantasticFiasco <mattias@kindb.org>
2021-05-14 17:27:38 +0200

    wip

--- test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Durable/FileSizeRolledBufferFilesShould.cs
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Durable/FileSizeRolledBufferFilesShould.cs
@@ -48,6 +48,49 @@ namespace Serilog.Sinks.Http.Private.Durable
         }
 
         [Fact]
+        public void HandleThreeDigitsDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "SomeBuffer-20001020.json",
+                "SomeBuffer-20001020_001.json",
+                "SomeBuffer-20001020_002.json",
+                "SomeBuffer-20001020_003.json",
+                "SomeBuffer-20001020_004.json",
+                "SomeBuffer-20001020_005.json",
+                "SomeBuffer-20001020_006.json",
+                "SomeBuffer-20001020_007.json",
+                "SomeBuffer-20001020_008.json",
+                "SomeBuffer-20001020_009.json",
+                "SomeBuffer-20001020_010.json",
+                // "txt" is used from >= v8
+                "SomeBuffer-20001020.txt",
+                "SomeBuffer-20001020_001.txt",
+                "SomeBuffer-20001020_002.txt",
+                "SomeBuffer-20001020_003.txt",
+                "SomeBuffer-20001020_004.txt",
+                "SomeBuffer-20001020_005.txt",
+                "SomeBuffer-20001020_006.txt",
+                "SomeBuffer-20001020_007.txt",
+                "SomeBuffer-20001020_008.txt",
+                "SomeBuffer-20001020_009.txt",
+                "SomeBuffer-20001020_010.txt"
+            };
+
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Fact]
         public void HandleFourDigits()
         {
             // Arrange
@@ -70,6 +113,33 @@ namespace Serilog.Sinks.Http.Private.Durable
         }
 
         [Fact]
+        public void HandleFourDigitsDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "SomeBuffer-20001020_999.json",
+                "SomeBuffer-20001020_1000.json",
+                "SomeBuffer-20001020_1001.json",
+                // "txt" is used from >= v8
+                "SomeBuffer-20001020_999.txt",
+                "SomeBuffer-20001020_1000.txt",
+                "SomeBuffer-20001020_1001.txt"
+            };
+
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Fact]
         public void HandleFiveDigits()
         {
             // Arrange
@@ -78,6 +148,33 @@ namespace Serilog.Sinks.Http.Private.Durable
                 "SomeBuffer-20001020_9999.json",
                 "SomeBuffer-20001020_10000.json",
                 "SomeBuffer-20001020_10001.json"
+            };
+
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
+
+        [Fact]
+        public void HandleFiveDigitsDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "SomeBuffer-20001020_9999.json",
+                "SomeBuffer-20001020_10000.json",
+                "SomeBuffer-20001020_10001.json",
+                // "txt" is used from >= v8
+                "SomeBuffer-20001020_9999.txt",
+                "SomeBuffer-20001020_10000.txt",
+                "SomeBuffer-20001020_10001.txt"
             };
 
             directoryService
