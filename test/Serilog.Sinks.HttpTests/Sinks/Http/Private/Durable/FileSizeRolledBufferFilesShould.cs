17255688cf5d292ed83e35e8c40ff6e5d44b9b72
FantasticFiasco <mattias@kindb.org>
2021-05-15 12:59:30 +0200

    wip

--- test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Durable/FileSizeRolledBufferFilesShould.cs
+++ test/Serilog.Sinks.HttpTests/Sinks/Http/Private/Durable/FileSizeRolledBufferFilesShould.cs
@@ -23,17 +23,17 @@ namespace Serilog.Sinks.Http.Private.Durable
             // Arrange
             var want = new[]
             {
-                "SomeBuffer-20001020.json",
-                "SomeBuffer-20001020_001.json",
-                "SomeBuffer-20001020_002.json",
-                "SomeBuffer-20001020_003.json",
-                "SomeBuffer-20001020_004.json",
-                "SomeBuffer-20001020_005.json",
-                "SomeBuffer-20001020_006.json",
-                "SomeBuffer-20001020_007.json",
-                "SomeBuffer-20001020_008.json",
-                "SomeBuffer-20001020_009.json",
-                "SomeBuffer-20001020_010.json"
+                "SomeBuffer-20001020.txt",
+                "SomeBuffer-20001020_001.txt",
+                "SomeBuffer-20001020_002.txt",
+                "SomeBuffer-20001020_003.txt",
+                "SomeBuffer-20001020_004.txt",
+                "SomeBuffer-20001020_005.txt",
+                "SomeBuffer-20001020_006.txt",
+                "SomeBuffer-20001020_007.txt",
+                "SomeBuffer-20001020_008.txt",
+                "SomeBuffer-20001020_009.txt",
+                "SomeBuffer-20001020_010.txt"
             };
 
             directoryService
@@ -47,145 +47,147 @@ namespace Serilog.Sinks.Http.Private.Durable
             got.ShouldBe(want);
         }
 
-        //[Fact]
-        //public void HandleThreeDigitsDuringV8Migration()
-        //{
-        //    // Arrange
-        //    var want = new[]
-        //    {
-        //        // "json" extension was used < v8
-        //        "SomeBuffer-20001020.json",
-        //        "SomeBuffer-20001020_001.json",
-        //        "SomeBuffer-20001020_002.json",
-        //        "SomeBuffer-20001020_003.json",
-        //        "SomeBuffer-20001020_004.json",
-        //        "SomeBuffer-20001020_005.json",
-        //        "SomeBuffer-20001020_006.json",
-        //        "SomeBuffer-20001020_007.json",
-        //        "SomeBuffer-20001020_008.json",
-        //        "SomeBuffer-20001020_009.json",
-        //        "SomeBuffer-20001020_010.json",
-        //        // "txt" is used from >= v8
-        //        "SomeBuffer-20001020.txt",
-        //        "SomeBuffer-20001020_001.txt",
-        //        "SomeBuffer-20001020_002.txt",
-        //        "SomeBuffer-20001020_003.txt",
-        //        "SomeBuffer-20001020_004.txt",
-        //        "SomeBuffer-20001020_005.txt",
-        //        "SomeBuffer-20001020_006.txt",
-        //        "SomeBuffer-20001020_007.txt",
-        //        "SomeBuffer-20001020_008.txt",
-        //        "SomeBuffer-20001020_009.txt",
-        //        "SomeBuffer-20001020_010.txt"
-        //    };
+        [Fact]
+        public void HandleThreeDigitSequenceNumbersDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "SomeBuffer-20001020.json",
+                "SomeBuffer-20001020_001.json",
+                "SomeBuffer-20001020_002.json",
+                "SomeBuffer-20001020_003.json",
+                "SomeBuffer-20001020_004.json",
+                "SomeBuffer-20001020_005.json",
+                "SomeBuffer-20001020_006.json",
+                "SomeBuffer-20001020_007.json",
+                "SomeBuffer-20001020_008.json",
+                "SomeBuffer-20001020_009.json",
+                "SomeBuffer-20001020_010.json",
+                // "txt" is used from >= v8
+                "SomeBuffer-20001020.txt",
+                "SomeBuffer-20001020_001.txt",
+                "SomeBuffer-20001020_002.txt",
+                "SomeBuffer-20001020_003.txt",
+                "SomeBuffer-20001020_004.txt",
+                "SomeBuffer-20001020_005.txt",
+                "SomeBuffer-20001020_006.txt",
+                "SomeBuffer-20001020_007.txt",
+                "SomeBuffer-20001020_008.txt",
+                "SomeBuffer-20001020_009.txt",
+                "SomeBuffer-20001020_010.txt"
+            };
 
-        //    directoryService
-        //        .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
-        //        .Returns(Randomize.Values(want));
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
 
-        //    // Act
-        //    var got = bufferFiles.Get();
+            // Act
+            var got = bufferFiles.Get();
 
-        //    // Assert
-        //    got.ShouldBe(want);
-        //}
+            // Assert
+            got.ShouldBe(want);
+        }
 
-        //[Fact]
-        //public void HandleFourDigits()
-        //{
-        //    // Arrange
-        //    var want = new[]
-        //    {
-        //        "SomeBuffer-20001020_999.json",
-        //        "SomeBuffer-20001020_1000.json",
-        //        "SomeBuffer-20001020_1001.json"
-        //    };
 
-        //    directoryService
-        //        .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
-        //        .Returns(Randomize.Values(want));
 
-        //    // Act
-        //    var got = bufferFiles.Get();
+        [Fact]
+        public void HandleFourDigitSequenceNumbers()
+        {
+            // Arrange
+            var want = new[]
+            {
+                "SomeBuffer-20001020_999.txt",
+                "SomeBuffer-20001020_1000.txt",
+                "SomeBuffer-20001020_1001.txt"
+            };
 
-        //    // Assert
-        //    got.ShouldBe(want);
-        //}
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
 
-        //[Fact]
-        //public void HandleFourDigitsDuringV8Migration()
-        //{
-        //    // Arrange
-        //    var want = new[]
-        //    {
-        //        // "json" extension was used < v8
-        //        "SomeBuffer-20001020_999.json",
-        //        "SomeBuffer-20001020_1000.json",
-        //        "SomeBuffer-20001020_1001.json",
-        //        // "txt" is used from >= v8
-        //        "SomeBuffer-20001020_999.txt",
-        //        "SomeBuffer-20001020_1000.txt",
-        //        "SomeBuffer-20001020_1001.txt"
-        //    };
+            // Act
+            var got = bufferFiles.Get();
 
-        //    directoryService
-        //        .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
-        //        .Returns(Randomize.Values(want));
+            // Assert
+            got.ShouldBe(want);
+        }
 
-        //    // Act
-        //    var got = bufferFiles.Get();
+        [Fact]
+        public void HandleFourDigitSequenceNumbersDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "SomeBuffer-20001020_999.json",
+                "SomeBuffer-20001020_1000.json",
+                "SomeBuffer-20001020_1001.json",
+                // "txt" is used from >= v8
+                "SomeBuffer-20001020.txt",
+                "SomeBuffer-20001020_001.txt",
+                "SomeBuffer-20001020_002.txt"
+            };
 
-        //    // Assert
-        //    got.ShouldBe(want);
-        //}
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
 
-        //[Fact]
-        //public void HandleFiveDigits()
-        //{
-        //    // Arrange
-        //    var want = new[]
-        //    {
-        //        "SomeBuffer-20001020_9999.json",
-        //        "SomeBuffer-20001020_10000.json",
-        //        "SomeBuffer-20001020_10001.json"
-        //    };
+            // Act
+            var got = bufferFiles.Get();
 
-        //    directoryService
-        //        .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
-        //        .Returns(Randomize.Values(want));
+            // Assert
+            got.ShouldBe(want);
+        }
 
-        //    // Act
-        //    var got = bufferFiles.Get();
+        [Fact]
+        public void HandleFiveDigitSequenceNumbers()
+        {
+            // Arrange
+            var want = new[]
+            {
+                "SomeBuffer-20001020_9999.txt",
+                "SomeBuffer-20001020_10000.txt",
+                "SomeBuffer-20001020_10001.txt"
+            };
 
-        //    // Assert
-        //    got.ShouldBe(want);
-        //}
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
 
-        //[Fact]
-        //public void HandleFiveDigitsDuringV8Migration()
-        //{
-        //    // Arrange
-        //    var want = new[]
-        //    {
-        //        // "json" extension was used < v8
-        //        "SomeBuffer-20001020_9999.json",
-        //        "SomeBuffer-20001020_10000.json",
-        //        "SomeBuffer-20001020_10001.json",
-        //        // "txt" is used from >= v8
-        //        "SomeBuffer-20001020_9999.txt",
-        //        "SomeBuffer-20001020_10000.txt",
-        //        "SomeBuffer-20001020_10001.txt"
-        //    };
+            // Act
+            var got = bufferFiles.Get();
 
-        //    directoryService
-        //        .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
-        //        .Returns(Randomize.Values(want));
+            // Assert
+            got.ShouldBe(want);
+        }
 
-        //    // Act
-        //    var got = bufferFiles.Get();
+        [Fact]
+        public void HandleFiveDigitsDuringV8Migration()
+        {
+            // Arrange
+            var want = new[]
+            {
+                // "json" extension was used < v8
+                "SomeBuffer-20001020_9999.json",
+                "SomeBuffer-20001020_10000.json",
+                "SomeBuffer-20001020_10001.json",
+                // "txt" is used from >= v8
+                "SomeBuffer-20001020_001.txt",
+                "SomeBuffer-20001020_002.txt",
+                "SomeBuffer-20001020_003.txt"
+            };
 
-        //    // Assert
-        //    got.ShouldBe(want);
-        //}
+            directoryService
+                .Setup(mock => mock.GetFiles(It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(Randomize.Values(want));
+
+            // Act
+            var got = bufferFiles.Get();
+
+            // Assert
+            got.ShouldBe(want);
+        }
     }
 }
