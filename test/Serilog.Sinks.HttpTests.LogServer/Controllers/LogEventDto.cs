d8f3a9bf184f6790f602efb7efffa244d1ede371
FantasticFiasco <mattias@kindb.org>
2021-11-08 08:10:20 +0100

    wip

--- test/Serilog.Sinks.HttpTests.LogServer/Controllers/LogEventDto.cs
+++ test/Serilog.Sinks.HttpTests.LogServer/Controllers/LogEventDto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using Serilog.Sinks.HttpTests.LogServer.Services;
 
 namespace Serilog.Sinks.HttpTests.LogServer.Controllers
@@ -7,16 +8,28 @@ namespace Serilog.Sinks.HttpTests.LogServer.Controllers
     {
         public DateTime Timestamp { get; set; }
 
+        public string Level { get; set; } = "Information";
+
+        public string? MessageTemplate { get; set; }
+
+        public string? RenderedMessage { get; set; }
+
+        public Dictionary<string, object>? Properties { get; set; }
+
         public LogEvent ToLogEvent()
         {
-            return new LogEvent(Timestamp);
+            return new LogEvent(Timestamp, Level, MessageTemplate, RenderedMessage, Properties);
         }
 
         public static LogEventDto From(LogEvent logEvent)
         {
             return new LogEventDto
             {
-                Timestamp = logEvent.Timestamp
+                Timestamp = logEvent.Timestamp,
+                Level = logEvent.Level,
+                MessageTemplate = logEvent.MessageTemplate,
+                RenderedMessage = logEvent.RenderedMessage,
+                Properties = logEvent.Properties
             };
         }
     }
--- test/Serilog.Sinks.HttpTests.LogServer/Services/LogEvent.cs
+++ test/Serilog.Sinks.HttpTests.LogServer/Services/LogEvent.cs
@@ -1,14 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Serilog.Sinks.HttpTests.LogServer.Services
 {
     public class LogEvent
     {
-        public LogEvent(DateTime timestamp)
+        public LogEvent(
+            DateTime timestamp,
+            string level,
+            string? messageTemplate,
+            string? renderedMessage,
+            Dictionary<string, object>? properties)
         {
             Timestamp = timestamp;
+            Level = level;
+            MessageTemplate = messageTemplate;
+            RenderedMessage = renderedMessage;
+            Properties = properties;
         }
 
         public DateTime Timestamp { get; }
+
+        public string Level { get; }
+
+        public string? MessageTemplate { get; }
+
+        public string? RenderedMessage { get; }
+
+        public Dictionary<string, object>? Properties { get; }
     }
 }
