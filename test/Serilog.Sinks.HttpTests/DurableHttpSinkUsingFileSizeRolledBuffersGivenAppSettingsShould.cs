695ffed86b72c5db33f7c8c44d1acdecaae4c604
FantasticFiasco <mattias@kindb.org>
2021-11-21 16:16:54 +0100

    wip

--- test/Serilog.Sinks.HttpTests/DurableHttpSinkUsingFileSizeRolledBuffersGivenAppSettingsShould.cs
+++ test/Serilog.Sinks.HttpTests/DurableHttpSinkUsingFileSizeRolledBuffersGivenAppSettingsShould.cs
@@ -1,25 +1,27 @@
-﻿using Microsoft.Extensions.Configuration;
-using Serilog.Core;
-using Serilog.Support.Fixtures;
-using Xunit;
+﻿//using Microsoft.Extensions.Configuration;
+//using Serilog.Core;
+//using Serilog.Support.Fixtures;
+//using Xunit;
 
-namespace Serilog
-{
-    public class DurableHttpSinkUsingFileSizeRolledBuffersGivenAppSettingsShould
-        : SinkFixture, IClassFixture<WebServerFixture>
-    {
-        public DurableHttpSinkUsingFileSizeRolledBuffersGivenAppSettingsShould(WebServerFixture webServerFixture)
-            : base(webServerFixture)
-        {
-            var configuration = new ConfigurationBuilder()
-                .AddJsonFile("appsettings_durable_http_using_file_size_rolled_buffers.json")
-                .Build();
+//namespace Serilog
+//{
+//    // TODO: Add test that congiguration is passed to HTTP client
+//
+//    public class DurableHttpSinkUsingFileSizeRolledBuffersGivenAppSettingsShould
+//        : SinkFixture, IClassFixture<WebServerFixture>
+//    {
+//        public DurableHttpSinkUsingFileSizeRolledBuffersGivenAppSettingsShould(WebServerFixture webServerFixture)
+//            : base(webServerFixture)
+//        {
+//            var configuration = new ConfigurationBuilder()
+//                .AddJsonFile("appsettings_durable_http_using_file_size_rolled_buffers.json")
+//                .Build();
 
-            Logger = new LoggerConfiguration()
-                .ReadFrom.Configuration(configuration)
-                .CreateLogger();
-        }
+//            Logger = new LoggerConfiguration()
+//                .ReadFrom.Configuration(configuration)
+//                .CreateLogger();
+//        }
 
-        protected override Logger Logger { get; }
-    }
-}
+//        protected override Logger Logger { get; }
+//    }
+//}
---
+++ test/Serilog.Sinks.HttpTests/Support/SerilogReflection.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Reflection;
+using Serilog.Core;
+using Serilog.Sinks.Http;
+using Serilog.Sinks.Http.Private.NonDurable;
+
+namespace Serilog.Support
+{
+    public static class SerilogReflection
+    {
+        public static HttpSinkReflection GetHttpSink(Logger logger)
+        {
+            var _sink = logger
+                .GetType()
+                .GetField("_sink", BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(logger);
+
+            if (_sink == null)
+            {
+                throw new Exception("Instance of type 'Logger' has no field called '_sink'.");
+            }
+
+            var _sinks = _sink
+                .GetType()
+                .GetField("_sinks", BindingFlags.Instance | BindingFlags.NonPublic)
+                .GetValue(_sink) as ILogEventSink[];
+
+            if (_sinks == null)
+            {
+                throw new Exception("Instance of type 'SafeAggregateSink' has no field called '_sinks'.");
+            }
+
+            if (_sinks.Length != 1)
+            {
+                throw new Exception("Instance of type 'SafeAggregateSink' contains more than one sink.");
+            }
+
+            var httpSink = _sinks[0] as HttpSink;
+            if (httpSink == null)
+            {
+                throw new Exception("Instance of type 'SafeAggregateSink' does not contain HttpSink.");
+            }
+
+            return new HttpSinkReflection(httpSink);
+        }
+
+        public class HttpSinkReflection
+        {
+            private readonly HttpSink httpSink;
+
+            public HttpSinkReflection(HttpSink httpSink)
+            {
+                this.httpSink = httpSink ?? throw new ArgumentNullException(nameof(httpSink));
+            }
+
+            public HttpSinkReflection SetRequestUri(string requestUri)
+            {
+                httpSink
+                    .GetType()
+                    .GetField("requestUri", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .SetValue(httpSink, requestUri);
+
+                return this;
+            }
+
+            public HttpSinkReflection SetHttpClient(IHttpClient httpClient)
+            {
+                httpSink
+                    .GetType()
+                    .GetField("httpClient", BindingFlags.Instance | BindingFlags.NonPublic)
+                    .SetValue(httpSink, httpClient);
+
+                return this;
+            }
+        }
+    }
+}
